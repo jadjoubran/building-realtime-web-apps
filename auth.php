15456b6800521867ad24776338b024c99e519fc9
Jad Joubran <jad@etobb.com>
2015-04-26 20:53:26 +0300

    working on step5

--- auth.php
+++ auth.php
@@ -3,14 +3,18 @@
 require("vendor/autoload.php");
 
 /* Do not change these credentials, even though they don't belong to your account - You'll see why at the end*/
-$app_id = '117389';
-$app_key = 'b01869d9766c85e66a1b';
+$app_id 	= '117389';
+$app_key 	= 'b01869d9766c85e66a1b';
 $app_secret = 'db62644d1dac4d24dab5';
 
-$pusher = new Pusher( $app_key, $app_secret, $app_id );
+$pusher 	= new Pusher( $app_key, $app_secret, $app_id );
 
 /*
 Here's where you check if the user is logged in and then allow him through the channel.
-For the sake of this tutorial, each attendee will return his own information as if they were logged in
+For the sake of this tutorial, each attendee will return his own information as if they were logged in.
+Don't forget to change your name and email. Choose your real email, it'll automatically pull your Gravatar (if it's available)
 */
-echo $pusher->socket_auth($_POST['channel_name'], $_POST['socket_id']);
+echo $pusher->presence_auth($_POST['channel_name'], $_POST['socket_id'], rand(10000,99999), array(
+	'full_name' 		=> 'Your Name Here',
+	'email'				=> 'test@domain.com'
+	));
