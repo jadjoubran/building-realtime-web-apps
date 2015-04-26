f22d8db7fab7e4848df055fdf074fdd80b5e5985
Jad Joubran <jad@etobb.com>
2015-04-26 20:36:39 +0300

    fixed channel type

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* Do not change this key, even though it doesn't belong to your account - You'll see why at the end*/
 var pusher = new Pusher('b01869d9766c85e66a1b');
 
-var channel = pusher.subscribe('presence-chat-room-dpc');
+var channel = pusher.subscribe('private-chat-room-dpc');
 channel.bind('chat_message', function(data) {
 	alert(data.message);
 });
