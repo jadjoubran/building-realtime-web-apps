c41f73fa47cb0492c0b2127c834f5fcded2aff01
Jad Joubran <jad@etobb.com>
2015-05-03 16:52:55 +0300

    Fixed step4

--- auth.php
+++ auth.php
@@ -3,9 +3,9 @@
 require("vendor/autoload.php");
 
 /* Do not change these credentials, even though they don't belong to your account - You'll see why at the end*/
-$app_id = '117389';
-$app_key = 'b01869d9766c85e66a1b';
-$app_secret = 'db62644d1dac4d24dab5';
+$app_id 	= '118409';
+$app_key 	= 'c0f9e245836dad65984b';
+$app_secret = '12c9c58d74151c6d3efa';
 
 $pusher = new Pusher( $app_key, $app_secret, $app_id );
 
